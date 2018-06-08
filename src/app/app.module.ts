e18824f55f883fa5abab799189c50feca3975f86
Anubhav Sharma <anubhavsharma960@gmail.com>
2018-06-08 17:11:19 +0530

    myapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, platformBrowser } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
-
+import { UserComponent }  from './components/user.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AboutComponent } from './components/about.component';
+import {routing} from './app.routing';
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule, HttpModule,routing ],
+  providers: [],
+  declarations: [ AppComponent,UserComponent, AboutComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
